4e526b1f96011cf001ffaca1834a7034cb154844
ErikM-T <erik.m.turrisystem@gmail.com>
2026-07-21 16:34:45 -0500

    Update Ejecutar_Script.bat

--- Ejecutar_Script.bat
+++ Ejecutar_Script.bat
@@ -1,10 +1,17 @@
 @echo off
-:: Solicitar permisos de Administrador automáticamente
+title Ejecutando Optimizador
+
+:: Verificar permisos de Administrador
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     powershell -Command "Start-Process '%~f0' -Verb RunAs"
     exit /b
 )
 
-:: Ejecutar el archivo .ps1 local
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0mi-script.ps1"
+:: Cambiar al directorio donde reside el archivo .bat
+cd /d "%~dp0"
+
+:: Ejecutar el script local
+powershell -NoProfile -ExecutionPolicy Bypass -File "mi-script.ps1"
+
+pause
